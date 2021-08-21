0ec5fbb6a4234ad5f9e6e66c27bfef7c586b772d
someoneonsmile <someoneonsmile@gmail.com>
2021-08-21 16:31:05 +0800

    option chain skip

--- rust-code/option_chain/src/main.rs
+++ rust-code/option_chain/src/main.rs
@@ -2,19 +2,21 @@ fn main() {
     println!("Hello, world!");
     let inner = map(None);
     println!("{:?}", inner);
-
 }
 
 fn map(out: Option<Out>) -> Option<Inner> {
-    out?.inner
+    let innner = out?.inner;
+    // while not print
+    println!("{:?}", "hello");
+    innner
 }
 
 #[derive(Debug)]
 struct Out {
-    inner: Option<Inner>
+    inner: Option<Inner>,
 }
 
 #[derive(Debug)]
 struct Inner {
-    msg: String
+    msg: String,
 }
